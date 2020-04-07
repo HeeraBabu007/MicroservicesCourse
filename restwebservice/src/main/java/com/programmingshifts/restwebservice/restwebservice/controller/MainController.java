496f3adeaf75729be3d0bbc034c035e2bc1712e0
Heera Babu <heerbabu@in.ibm.com>
2020-04-08 02:01:29 +0530

    product API  created with model and controller

--- restwebservice/src/main/java/com/programmingshifts/restwebservice/restwebservice/controller/MainController.java
+++ restwebservice/src/main/java/com/programmingshifts/restwebservice/restwebservice/controller/MainController.java
@@ -1,9 +1,12 @@
 package com.programmingshifts.restwebservice.restwebservice.controller;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.programmingshifts.restwebservice.restwebservice.model.Product;
 
 @RestController
 public class MainController {
@@ -11,6 +14,11 @@ public class MainController {
 	@GetMapping("/greetings")
 	public String GreetingMethod() {
 		return "Hello World";
-
+	}
+	
+	@GetMapping("/products")
+	public List<Product> getAllProducts() {
+		return Arrays.asList(
+				new Product(1l, "Spring Boot Micro-Service Ebook", "Written By Author: Er Heera Babu."));
 	}
 }
