1c0b16ec7f4b81b3fd4018219065a76b3a8d249b
Heera Babu <heerbabu@in.ibm.com>
2020-04-08 01:55:09 +0530

    product class added in model package

---
+++ restwebservice/src/main/java/com/programmingshifts/restwebservice/restwebservice/model/Product.java
@@ -0,0 +1,37 @@
+package com.programmingshifts.restwebservice.restwebservice.model;
+
+public class Product {
+
+	long id;
+	String name;
+	String description;
+
+	public Product(long id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+
+}
